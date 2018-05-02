2edb7508b1211d49b9af490944a1a02d6befdd2a
wiltrahan <twilorip@gmail.com>
2018-05-02 19:01:18 -0400

    config for login form

--- src/java/com/portfolioscraper/springsecurity/conifg/SecurityConfig.java
+++ src/java/com/portfolioscraper/springsecurity/conifg/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.portfolioscraper.springsecurity.conifg;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,6 +24,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("jess").password("test333").roles("user"))
 			.withUser(users.username("willis").password("test1111").roles("user"));
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll();
+	
+	}
+	
+	
 
 	
 }
